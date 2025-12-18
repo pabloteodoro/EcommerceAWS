3232b4c310942cf6d5348eb95875db6581d2fae3
Pablo Teodoro <pabloteadoro123@gmail.com>
2025-12-18 19:33:32 -0300

    Creating the code for the product change event generation function.

---
+++ lambda/products/productEventsFunction.ts
@@ -0,0 +1,47 @@
+import { Callback, Context } from "aws-lambda";
+import { DynamoDB } from "aws-sdk";
+import * as AWSXRay from "aws-xray-sdk"
+import { ProductEvent } from "/opt/nodejs/productEventsLayer";
+
+AWSXRay.captureAWS(require("aws-sdk"))
+
+const eventsDdb = process.env.EVENTS_DDB!
+const ddbClient = new DynamoDB.DocumentClient()
+
+
+export async function handler(event: ProductEvent, context: Context, callback: Callback): Promise<void> {
+
+    console.log(event)
+
+    console.log(`Lambda requestId: ${context.awsRequestId}`)
+
+    await createEvent(event)
+    
+    callback(null, JSON.stringify({
+        productEventCreated: true,
+        message: "Event created successfully"
+    }))
+
+}
+
+function createEvent(event: ProductEvent) {
+    const timestamp = Date.now()
+    const ttl = ~~(timestamp / 1000 + 5 * 60)
+
+    return ddbClient.put({
+        TableName: eventsDdb,
+        Item: {
+            pk: `#product_${event.productCode}`,
+            sk: `${event.eventType}#${timestamp}`,
+            email: event.email,
+            createdAt: timestamp,
+            requestId: event.requestId,
+            eventType: event.eventType,
+            info: {
+                productId: event.productId,
+                price: event.productPrice
+            },
+            ttl: ttl
+        }
+    }).promise()
+}
